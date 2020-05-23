8236a768a5453135ed288f35b3f14325c65c1e96
xeken <xeken@naver.com>
2020-05-23 16:17:22 +0900

    wpf

---
+++ Wpf/exicon_DataExchange/exicon_DataExchange/ICommandImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace exicon_DataExchange
+{
+    public class ICommandImpl : ICommand
+    {
+        Action<object> _executeMethod;
+        Func<object, bool> _canexecuteMethod;
+
+        public ICommandImpl(Action<object> executeMethod, Func<object, bool> canexecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canexecuteMethod = canexecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }
+        private bool CanExecuteMethod(object arg)
+        {
+            return true;
+        }
+    }
+}
---
+++ Wpf/exicon_DataExchange/exicon_DataExchange/ViewModel/ViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace exicon_DataExchange.ViewModel
+{
+    public class ViewModel : INotifyPropertyChangedImpl
+    {
+        public ICommand MoveToRightCommand { get; set; }
+        public ICommand MoveToLeftCommand { get; set; }
+        private bool CanExecuteMethod(object arg) => true;
+        private void MoveToRight(object obj)
+        {
+            LogText += SelectedItems.Count;
+        }
+        private void MoveToLeft(object obj)
+        {
+            LogText += "EEEEEE\n";
+        }
+
+        private List<String> wordList1;
+        public List<String> WordList1
+        {
+            get => this.wordList1;
+            set
+            {
+                this.wordList1 = value;
+                OnPropertyChanged("WordList1");
+            }
+        }
+
+        private List<String> wordList2;
+        public List<String> WordList2
+        {
+            get => this.wordList2;
+            set
+            {
+                this.wordList2 = value;
+                OnPropertyChanged("WordList2");
+            }
+        }
+
+        //private ObservableCollection<ItemPresenter> SelectedItems { get; } = new ObservableCollection<ItemPresenter>();
+        ////public ObservableCollection<ItemPresenter> SelectedItems
+        ////{
+        ////    get => this.selectedItems;
+        ////    set
+        ////    {
+        ////        this.selectedItems = value;
+        ////        OnPropertyChanged("SelectedItems");
+        ////    }
+        ////}
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get => this.isSelected;
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+        private String logText;
+        public String LogText
+        {
+            get => this.logText;
+            set
+            {
+                this.logText = value;
+                OnPropertyChanged("LogText");
+            }
+        }
+
+        public ViewModel()
+        {
+            WordList1 = new List<string>();
+            WordList1.Add("Hello");
+            WordList1.Add("World");
+            WordList1.Add("Fox");
+            WordList1.Add("Jumps over");
+            WordList1.Add("Rabbit");
+
+            WordList2 = new List<string>();
+            WordList2.Add("Howdy");
+            WordList2.Add("Hero");
+
+            MoveToRightCommand = new ICommandImpl(MoveToRight, CanExecuteMethod);
+            MoveToLeftCommand = new ICommandImpl(MoveToLeft, CanExecuteMethod);
+        }
+    }
+    public class ItemPresenter : INotifyPropertyChangedImpl
+    {
+        private readonly string _value;
+
+        public ItemPresenter(string value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
